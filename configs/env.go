6610aaf4a075463108ca8477977116e72a4b0f35
D'Riski Maulana <maulanadriski@gmail.com>
2023-07-06 15:36:46 +0700

    remove env

--- configs/env.go
+++ configs/env.go
@@ -1,16 +1,14 @@
 package configs
 
 import (
-	"github.com/joho/godotenv"
-	"log"
 	"os"
 )
 
 func EnvMongoURI() string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file.")
-	}
+	//err := godotenv.Load()
+	//if err != nil {
+	//	log.Fatal("Error loading .env file.")
+	//}
 
 	return os.Getenv("MONGOURI")
 }
